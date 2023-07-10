5c53e04d7036625af3978246f0831c2fe66bf52f
patdixan <patdixan@sheridancollege.ca>
2023-07-10 14:11:02 -0700

    Version 1

--- src/arithmetic/Arithmetic.java
+++ src/arithmetic/Arithmetic.java
@@ -5,32 +5,36 @@
  */
 package arithmetic;
 
-
 import java.util.Scanner;
 import static java.time.Clock.system;
 
-/** This class calls the method to perform 
- * arithmetic operations based on user input
- * execute the code check the output
+/**
+ * This class calls the method to perform arithmetic operations based on user
+ * input execute the code check the output
+ *
  * @author sivagamasrinivasan
- * 
+ *
  */
-public class Arithmetic 
-{
+public class Arithmetic {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) 
-    {
-       
-        ArithmeticBase r= new ArithmeticBase();
-        Scanner in= new Scanner(System.in);
-        int n= in.nextInt();
-        int m= in.nextInt();
-        double result = r.calculate(m,n);
-        System.out.println("result :" +result); 
-    
+    public static void main(String[] args) {
+
+        ArithmeticBase r = new ArithmeticBase();
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter two numbers: ");
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        System.out.println("Enter arithmetic operation to perform: ");
+        String operationStr = in.next();
+        Operation operation = Operation.valueOf(operationStr.toUpperCase());
+
+        double result = r.calculate(n, m, operation);
+        System.out.println("result :" + result);
+
     }
 }
-
--- src/arithmetic/ArithmeticBase.java
+++ src/arithmetic/ArithmeticBase.java
@@ -7,33 +7,38 @@ package arithmetic;
 
 import java.util.Scanner;
 
-/** This class takes String input plus,minus,divide and times
- * from user and execute the arithmetic operation
- * change the code to use enum instead String and mention the advantage of enum.
+/**
+ * This class takes String input plus,minus,divide and times from user and
+ * execute the arithmetic operation change the code to use enum instead String
+ * and mention the advantage of enum.
+ *
  * @author sivagamasrinivasan
- * 
+ *
+ * @modifier Dixantkumar Patel
+ *
  */
-public class ArithmeticBase 
-{
- public double x,y;
-    double calculate(double x, double y) 
-        {
-        Scanner sc =new Scanner(System.in);
-        System.out.println("Enter arithmetic operation to Perform: ");
-        String s= sc.next();
-        switch (s.toUpperCase()) 
-        {
-            case "PLUS":
+enum Operation {
+    PLUS,
+    MINUS,
+    TIMES,
+    DIVIDE
+}
+
+public class ArithmeticBase {
+
+    public double calculate(double x, double y, Operation operation) {
+        switch (operation) {
+            case PLUS:
                 return x + y;
-            case "MINUS":
+            case MINUS:
                 return x - y;
-            case "TIMES":
+            case TIMES:
                 return x * y;
-            case "DIVIDE":
+            case DIVIDE:
                 return x / y;
             default:
-                throw new AssertionError("Unknown operations " + this);
+                throw new AssertionError("Unknown operation: " + operation);
         }
     }
-   
+
 }
